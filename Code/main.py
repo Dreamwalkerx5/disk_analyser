145c4e452bc2852e9205a6f51f15173353276953
Steven Taylor <43670847+Dreamwalkerx5@users.noreply.github.com>
2019-07-30 10:25:13 +0100

    main creation

---
+++ Code/main.py
@@ -0,0 +1,10 @@
+# Disk Analyser v1.0
+# Copyright (c) 2019 Steven Taylor All rights reserved
+
+from PyQt5 import QtWidgets, QtGui, QtCore
+
+
+class Gui(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(Gui, self).__init__()
