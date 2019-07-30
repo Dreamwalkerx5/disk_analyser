a63221114fe284bf909c904b5da1d892e16e947c
Steven Taylor <43670847+Dreamwalkerx5@users.noreply.github.com>
2019-07-30 15:28:20 +0100

    Database class

--- Code/database.py
+++ Code/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import sys
 
 
 class Database:
@@ -7,7 +8,25 @@ class Database:
         self.db = self.open_database()
         self.gui = gui
 
-    def open_database(self, gui):
+    def clear_database(self):
+
+        # Delete old database
+        try:
+            c = self.db.cursor()
+            c.execute('DROP TABLE IF EXISTS DiskTree ')
+            self.db.commit()
+
+            # Create new database
+            Database.create_new_database(self.db)
+            self.gui.info_label.setText('Database has been cleared.')
+
+        except:
+
+            print("Unexpected error:", sys.exc_info()[0])
+            print(sys.exc_info()[1])
+
+    @staticmethod
+    def open_database(gui):
 
         # open a connection to sqlite3
         db = sqlite3.connect('Disk.db')
@@ -16,19 +35,24 @@ class Database:
         c = db.cursor()
         c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='DiskTree' ''')
         if not c.fetchone()[0]:
-            Database.create_new_database(gui, db)
+
+            Database.create_new_database(db)
+            gui.info_label.setText('Database created and a connection made.')
+
         else:
+
             gui.info_label.setText('Database found and a connection made.')
 
         db.commit()
 
         return db
 
-    def create_new_database(self, gui, db):
+    @staticmethod
+    def create_new_database(db):
+
         c = db.cursor()
-        c.execute('''CREATE TABLE journal(id INTEGER PRIMARY KEY, created TIMESTAMP, category TEXT, entry TEXT)''')
-        c.execute('''CREATE TABLE info(id INTEGER PRIMARY KEY, created TIMESTAMP, keywords TEXT, info TEXT)''')
+        c.execute('''CREATE TABLE DiskTree(id INTEGER PRIMARY KEY, created TIMESTAMP, category TEXT, entry TEXT)''')
         db.commit()
 
         print("New database created.")
-        gui.info_label.setText('New database created and a connection made')
+
